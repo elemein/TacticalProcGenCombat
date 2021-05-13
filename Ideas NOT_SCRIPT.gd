9c614863c10181449bd2191bd72caca323af006b
elemein <44098408+elemein@users.noreply.github.com>
2021-05-13 16:03:29 -0400

    Added idea pad script to record ideas.

---
+++ Ideas NOT_SCRIPT.gd
@@ -0,0 +1,23 @@
+"""
+Use this script for ideas:
+	
+ We like:
+- Single Player (not built for, not built against)
+- Procgen
+- Tactical Combat/Not RealTime
+- Tile-Based
+- Top Down/Isometric 
+- 3D Engine / 2D Sprites
+- Fantasy, (No Time Limit)
+- Building Base w/ Rewards from Dungeon
+
+Result: Make a mystery dungeon game,
+
+Inspirations: Zettai Hero Project, Pokemon Mystery Dungeon, Izuna: Unemployed Ninja
+
+
+Possible Longterm Directions?:
+	- Single player story based, like ZHP and most existing Mystery Dungeon games.
+	- Multi player based? It doesnt look like theres very many games like this from my searches.
+
+"""
