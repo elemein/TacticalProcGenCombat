2198909daf9bfe561267aa788bb909eaa46f62c6
elemein <44098408+elemein@users.noreply.github.com>
2021-05-14 13:15:19 -0400

    Changed directions, moving. Added basic attack. Maps now generate based on map.

--- Player.gd
+++ Player.gd
@@ -1,87 +1,142 @@
 extends KinematicBody
 
-const MOVE_SPEED = 130
-const JUMP_FORCE = 30
-const GRAVITY = 0.98
-const MAX_FALL_SPEED = 30
+const MOVE_SPEED = 2.2
+const DIRECTION_SELECT_TIME = 0.3
 
-const H_LOOK_SENS = 0.01
-const V_LOOK_SENS = 0.2
-
-onready var cam = $CamBase
+onready var model = $Graphics
 onready var anim = $Graphics/AnimationPlayer
-
-var y_velo = 0
+onready var gui = get_node("/root/World/GUI")
+onready var turn_timer = get_node("/root/World/TurnTimer")
 
 # Used to store the action for the next turn.
 var proposed_action = ""
 
+var ready_status = false
+
+var direction_facing = "down"
+var directional_timer = Timer.new()
+var still_deciding_direction = false
+
+var anim_state = "idle"
+
+var saved_pos = Vector3()
+var target_pos = Vector3()
+
 func _ready():
-	anim.get_animation("walk").set_loop(true)
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-#func _input(event):
-#	if event is InputEventMouseMotion:
-#		rotation.y -= event.relative.x * H_LOOK_SENS
+	directional_timer.set_one_shot(true)
+	directional_timer.set_wait_time(DIRECTION_SELECT_TIME)
+	directional_timer.connect("timeout", self, "on_direction_timer_complete")
+	add_child(directional_timer)
 		
-func handle_animations(just_jumped, grounded, move_vec):
-	if just_jumped:
-		play_anim("jump")
-	elif grounded:
-		if move_vec.x ==0 and move_vec.z == 0:
-			play_anim("idle")
-		else:
-			play_anim("walk")
-		
-func get_movement_transforms():
-	var move_vec = Vector3()
-	
-	if Input.is_action_just_pressed("move_forwards"):
-		move_vec.z -= 1
-		proposed_action = "move up"
-	if Input.is_action_just_pressed("move_backwards"):
-		move_vec.z += 1
-		proposed_action = "move down"
-	if Input.is_action_just_pressed("move_left"):
-		move_vec.x -= 1	
-		proposed_action = "move left"
-	if Input.is_action_just_pressed("move_right"):
-		move_vec.x += 1
-		proposed_action = "move right"
-	return move_vec
-	
-func _physics_process(_delta):
-	# Get movement vector from input, apply it,
-	var move_vec = get_movement_transforms().normalized()
-	
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_vec *= MOVE_SPEED
-	move_vec.y = y_velo
-	
-	move_and_slide(move_vec, Vector3(0, 1, 0))
-	
-	# Handles jumping. ----------- Not sure we need this.
-	var grounded = is_on_floor()
-	y_velo -= GRAVITY
-	var just_jumped = false
-	
-	if grounded and Input.is_action_just_pressed("jump"):
-		just_jumped = true
-		y_velo = JUMP_FORCE
-	if grounded and y_velo <= 0:
-		y_velo = -0.1
-	if y_velo < -MAX_FALL_SPEED:
-		y_velo = -MAX_FALL_SPEED
-	# ----------------------------
-	
-	handle_animations(just_jumped, grounded, move_vec)
-	
 func play_anim(name):
 	if anim.current_animation == name:
 		return
 	anim.play(name)
 
+func get_input():
+	if turn_timer.time_left > 0: # We don't wanna collect input if turn in action.
+		return
 	
+	# Below sets direction. It checks for the directional key being used, AND
+	# if the char is not already facing that direction, and then starts the 
+	# timer to decide direction.
+	if Input.is_action_pressed("w") && direction_facing != 'up':
+		model.rotation_degrees.y = 90
+		direction_facing = "up"
+		still_deciding_direction = true
+		directional_timer.start(DIRECTION_SELECT_TIME) 
+	if Input.is_action_pressed("s") && direction_facing != 'down':
+		model.rotation_degrees.y = 90 + 180
+		direction_facing = "down"
+		still_deciding_direction = true
+		directional_timer.start(DIRECTION_SELECT_TIME)
+	if Input.is_action_pressed("a") && direction_facing != 'left':
+		model.rotation_degrees.y = 180
+		direction_facing = "left"
+		still_deciding_direction = true
+		directional_timer.start(DIRECTION_SELECT_TIME)
+	if Input.is_action_pressed("d") && direction_facing != 'right':
+		model.rotation_degrees.y = 180 + 180
+		direction_facing = "right"
+		still_deciding_direction = true
+		directional_timer.start(DIRECTION_SELECT_TIME)
+
+	# As the move buttons are used to change direction, these need to abide
+	# to the directional timer.
+	if still_deciding_direction == false:
+		if Input.is_action_pressed("w"):
+			proposed_action = "move up"
+			gui.propose_action(proposed_action)
+			ready_status = true
+		if Input.is_action_pressed("s"):
+			proposed_action = "move down"
+			gui.propose_action(proposed_action)
+			ready_status = true
+		if Input.is_action_pressed("a"):
+			proposed_action = "move left"
+			gui.propose_action(proposed_action)
+			ready_status = true
+		if Input.is_action_pressed("d"):
+			proposed_action = "move right"
+			gui.propose_action(proposed_action)
+			ready_status = true
 	
+	# Basic attacks only need one press.
+	if Input.is_action_pressed("space"):
+		proposed_action = "basic attack"
+		gui.propose_action(proposed_action)
+		ready_status = true
 	
+	# Skills will need two taps to confirm.
+	pass
+
+func on_direction_timer_complete():
+	still_deciding_direction = false	
+		
+func process_turn():
 	
+	ready_status = false
+	target_pos = translation # Reset this.
+	
+	if proposed_action.split(" ")[0] == 'move' || proposed_action == 'basic attack':
+		match direction_facing:
+			'up':
+				target_pos.x = translation.x + MOVE_SPEED
+			'down':
+				target_pos.x = translation.x + -MOVE_SPEED
+			'left':
+				target_pos.z = translation.z + -MOVE_SPEED
+			'right':
+				target_pos.z = translation.z + MOVE_SPEED
+		
+func end_turn():
+	# Clear action.
+	saved_pos = translation
+	proposed_action = ''
+
+func _physics_process(_delta):
+	
+	# Change position based on time tickdown.
+	if proposed_action.split(" ")[0] == 'move':
+		translation = translation.linear_interpolate(target_pos, (1-turn_timer.time_left)) 
+	
+	if proposed_action == "basic attack":
+		if turn_timer.time_left > 0.5: # Move char towards attack cell.
+			translation = translation.linear_interpolate(target_pos, (1-(turn_timer.time_left - 0.5))) 
+		else: # Move char back.
+			translation = translation.linear_interpolate(saved_pos, (0.5-turn_timer.time_left))
+	
+	if proposed_action != '':
+		anim_state = "walk"
+	else:
+		anim_state = "idle"
+	
+	get_input()
+	
+	handle_animations()
+
+func handle_animations():
+	if anim_state == "idle":
+		play_anim("idle")
+	else:
+		play_anim("walk")
---
+++ TurnTimer.gd
@@ -0,0 +1,38 @@
+extends Timer
+
+onready var player = get_node("/root/World/Player")
+
+var turn_counter = 1
+
+# Use this to hold all of the actors in the world; players, enemies, etc.
+var actors = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	actors.append(player)
+
+func process_turn():
+	turn_counter += 1
+	
+	start()
+	
+	for actor in actors:
+		actor.process_turn()
+	
+func end_turn():
+	for actor in actors:
+		actor.end_turn()
+
+func _physics_process(_delta):
+	var all_ready = true
+	
+	for actor in actors:
+		if actor.ready_status == false:
+			all_ready = false
+
+	if all_ready:
+		process_turn()
+
+
+func _on_TurnTimer_timeout():
+	end_turn()
