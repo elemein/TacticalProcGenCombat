52bb94844c6878f2ffc5ca220e2b6e164273334f
elemein <44098408+elemein@users.noreply.github.com>
2021-05-14 13:15:19 -0400

    Changed directions, moving. Added basic attack. Maps now generate based on map.

--- Map.gd
+++ Map.gd
@@ -2,50 +2,18 @@ extends Node
 
 const Y_OFFSET = -0.3
 const TILE_OFFSET = 2.2
-const MAX_NUMBER_OF_ENEMIES = 2
 
 var base_block = preload("res://BaseBlock.tscn")
-var base_enemy = preload("res://Enemy.tscn")
 
 # MAP is meant to be accessed via [x][z] where '0' is a blank tile.
-var map_x = 5
-var map_z = 7
-var map_grid = []
-#var map_grid = [['0', '0', '0', '0', '0', '0', '0', '0'],
-#				['0', '0', '0', '0', '0', '0', '0', '0'],
-#				['0', '0', '0', '0', '0', '0', '0', '0'],
-#				['0', '0', '0', '0', '0', '0', '0', '0'],]
-
-var current_number_of_enemies = 0
-
-func create_empty_map():
-	# create the map based on the map_x and map_y variables
-	var map_width = []
-	for x in map_x:
-		map_width.append('0')
-		
-	for z in map_z:
-		map_grid.append(map_width.duplicate())
-
-func spawn_enemies():
-	for enemy_cnt in MAX_NUMBER_OF_ENEMIES:
-		
-		# Get random x/y coords to spawn enemy
-		var enemy_x = 0
-		var enemy_z = 0
-		while enemy_x == 0 and enemy_z == 0 and map_grid[enemy_x][enemy_z] != '1':
-			enemy_x = randi() % map_grid.size()
-			enemy_z = randi() % map_grid[enemy_x].size()
-		map_grid[enemy_x][enemy_z] = '1'
-		current_number_of_enemies += 1
+var map_grid = [['0', '0', '0', '0', '0', '0', '0', '0'],
+				['0', '0', '0', '0', '0', '0', '0', '0'],
+				['0', '0', '0', '0', '0', '0', '0', '0'],
+				['0', '0', '0', '0', '0', '0', '0', '0'],]
 
 func _ready():
-	randomize()
 	var x_offset = -TILE_OFFSET
 	var z_offset = -TILE_OFFSET
-	
-	create_empty_map()
-	spawn_enemies()
 	
 	for x_coord in map_grid:
 		x_offset += TILE_OFFSET
@@ -56,9 +24,3 @@ func _ready():
 			var block = base_block.instance()
 			add_child(block)
 			block.translation = Vector3(x_offset, Y_OFFSET, z_offset)
-			
-			if z_coord == '1':
-				var enemy = base_enemy.instance()
-				add_child(enemy)
-				enemy.translation = Vector3(x_offset, Y_OFFSET+0.3, z_offset)
-				enemy.add_to_group('enemies')
--- TurnTimer.gd
+++ TurnTimer.gd
@@ -1,44 +1,34 @@
 extends Timer
 
 onready var player = get_node("/root/World/Player")
-onready var enemy = get_node("/root/World/Enemy")
 
 var turn_counter = 1
 
 # Use this to hold all of the actors in the world; players, enemies, etc.
-var actors = {}
+var actors = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	actors['player'] = [player]
-	actors['enemies'] = [enemy]
+	actors.append(player)
 
 func process_turn():
 	turn_counter += 1
 	
 	start() #This starts the timer on self.
 	
-	for category in actors:
-		if category == 'player':
-			for actor in actors[category]:
-				actor.process_turn()
-		elif category == 'enemy':
-			for actor in actors[category]:
-#				actor.process_turn()
-				pass
+	for actor in actors:
+		actor.process_turn()
 	
 func end_turn():
-	for category in actors:
-		for actor in actors[category]:
-			actor.end_turn()
+	for actor in actors:
+		actor.end_turn()
 
 func _physics_process(_delta):
 	var all_ready = true
 	
-	for category in actors:
-		for actor in actors[category]:
-			if actor.ready_status == false:
-				all_ready = false
+	for actor in actors:
+		if actor.ready_status == false:
+			all_ready = false
 
 	if all_ready:
 		process_turn()
