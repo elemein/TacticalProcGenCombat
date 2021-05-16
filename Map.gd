39130ff21ee2b47cf55835bb1f67a7daba4f0234
elemein <44098408+elemein@users.noreply.github.com>
2021-05-16 13:33:37 -0400

    Changed file structure. Player now unreliably tracks to map.

--- Map.gd
+++ Map.gd
@@ -3,7 +3,7 @@ extends Node
 const Y_OFFSET = -0.3
 const TILE_OFFSET = 2.2
 
-var base_block = preload("res://BaseBlock.tscn")
+var base_block = preload("res://Assets/Objects/MapObjects/BaseBlock.tscn")
 
 # MAP is meant to be accessed via [x][z] where '0' is a blank tile.
 var map_grid = [['0', '0', '0', '0', '0', '0', '0', '0'],
@@ -24,3 +24,26 @@ func _ready():
 			var block = base_block.instance()
 			add_child(block)
 			block.translation = Vector3(x_offset, Y_OFFSET, z_offset)
+
+func place_on_map(object, curr_pos):
+	var x_pos = int(curr_pos.x/TILE_OFFSET)
+	var z_pos = int(curr_pos.z/TILE_OFFSET)
+	map_grid[x_pos][z_pos] = object
+	
+	return [x_pos, z_pos]
+
+func move_on_map(object, old_pos, new_pos):
+	# Place object at new location.
+	var new_x_pos = int(stepify(new_pos.x, 0.1)/TILE_OFFSET)
+	var new_z_pos = int(stepify(new_pos.z, 0.1)/TILE_OFFSET)
+	print ([int(new_pos.x/TILE_OFFSET), int(new_pos.z/TILE_OFFSET)])
+	map_grid[new_x_pos][new_z_pos] = object
+	# Clear old location.
+	var old_x_pos = int(stepify(old_pos.x, 0.1)/TILE_OFFSET)
+	var old_z_pos = int(stepify(old_pos.z, 0.1)/TILE_OFFSET)
+	map_grid[old_x_pos][old_z_pos] = '0'
+	
+	for line in map_grid:
+		print(line)
+		
+	return [new_x_pos, new_z_pos]
