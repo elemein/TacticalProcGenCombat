a7a3a3f7001fbbe6369995e616aa86ba68fd7568
Debonairesnake6 <canadianboyz@hotmail.ca>
2021-05-16 16:58:52 -0400

    First commit
    Updated map creation. Fixed walking animation after stopped moving. Added basic enemy. Randomly spawn enemies.

--- TurnTimer.gd
+++ TurnTimer.gd
@@ -1,34 +1,44 @@
 extends Timer
 
 onready var player = get_node("/root/World/Player")
+onready var enemy = get_node("/root/World/Enemy")
 
 var turn_counter = 1
 
 # Use this to hold all of the actors in the world; players, enemies, etc.
-var actors = []
+var actors = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	actors.append(player)
+	actors['player'] = [player]
+	actors['enemies'] = [enemy]
 
 func process_turn():
 	turn_counter += 1
 	
 	start()
 	
-	for actor in actors:
-		actor.process_turn()
+	for category in actors:
+		if category == 'player':
+			for actor in actors[category]:
+				actor.process_turn()
+		elif category == 'enemy':
+			for actor in actors[category]:
+#				actor.process_turn()
+				pass
 	
 func end_turn():
-	for actor in actors:
-		actor.end_turn()
+	for category in actors:
+		for actor in actors[category]:
+			actor.end_turn()
 
 func _physics_process(_delta):
 	var all_ready = true
 	
-	for actor in actors:
-		if actor.ready_status == false:
-			all_ready = false
+	for category in actors:
+		for actor in actors[category]:
+			if actor.ready_status == false:
+				all_ready = false
 
 	if all_ready:
 		process_turn()
