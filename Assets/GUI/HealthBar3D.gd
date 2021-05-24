e06417e655cd5e1c4f42b9b3bc7a2022c5df10c4
Debonairesnake6 <canadianboyz@hotmail.ca>
2021-05-23 21:34:17 -0400

    Added mana bar. Player now also has a health bar. Player now has max mana and can run oom. Added sound effect for running oom.

--- Assets/GUI/HealthBar3D.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite3D
-
-onready var health_bar = $Viewport/HealthBar2D
-
-func _ready():
-	texture = $Viewport.get_texture()
-
-func update(value, full):
-	health_bar.update_bar(value, full)
