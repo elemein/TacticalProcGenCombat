2c0e5d10d255b42b8f7262c918b43c0bdf844091
elemein <44098408+elemein@users.noreply.github.com>
2021-05-24 21:29:25 -0400

    Pathfinder now returns shortest path.

--- Assets/SystemScripts/PathFinder.gd
+++ Assets/SystemScripts/PathFinder.gd
@@ -8,15 +8,17 @@ onready var map = get_node("/root/World/Map")
 
 var visited = []
 
-var start_row = 0
-var start_col = 0
 
 var target_pos = 0
 
 var reached_end
 
+var start = []
+
 var row_queue = []
 var col_queue = []
+
+var path_holder = {}
 
 var pos_queue = []
 
@@ -24,12 +26,7 @@ var move_count = 0
 var nodes_left_in_layer = 0
 var nodes_in_next_layer = 0
 
-func solve(searcher, start_pos, end_pos):
-	start_row = start_pos[0]
-	start_col = start_pos[1]
-	
-	target_pos = end_pos
-	
+func reset_vars():
 	pos_queue = []
 
 	move_count = 0
@@ -38,14 +35,19 @@ func solve(searcher, start_pos, end_pos):
 
 	reached_end = false
 
+	path_holder = {}
+
 	visited = []
+	
+	
+
+func solve(searcher, start_pos, end_pos):
+	reset_vars()
+	start = start_pos
+	target_pos = end_pos
 	
 	pos_queue.push_front(start_pos)
 	visited.append(start_pos)
-	
-	print([start_pos, end_pos])
-	
-	var total_counts = []
 	
 	# Solving ------------------------------------
 	while pos_queue.size() > 0:
@@ -53,7 +55,6 @@ func solve(searcher, start_pos, end_pos):
 		
 		if curr_pos == target_pos:
 			reached_end = true
-			total_counts.append(move_count)
 			break
 		
 		explore_neighbors(curr_pos)
@@ -65,11 +66,13 @@ func solve(searcher, start_pos, end_pos):
 			move_count += 1
 	
 	if reached_end:
-		print("%s 's distance to Player: %s via %s" % [searcher, move_count, visited])
+		retrace_path()
 		return move_count
+
 	return -1
 			
-func explore_neighbors(pos):
+func explore_neighbors(pos): # this function basically just adds adjacent tiles to queue if its valid
+	path_holder[pos] = []
 	for direction in ['up', 'down', 'left','right', 
 						'upleft', 'upright', 'downleft', 'downright']:
 				
@@ -89,8 +92,6 @@ func explore_neighbors(pos):
 				
 		tile_contents = map.get_tile_contents(search_pos[0], search_pos[1])
 		
-		if search_pos == target_pos: break
-		
 		if search_pos in visited: continue
 		if typeof(tile_contents) == TYPE_STRING:
 			if tile_contents == 'Out of Bounds': continue
@@ -98,7 +99,21 @@ func explore_neighbors(pos):
 			
 		pos_queue.push_front(search_pos)
 		
-		
 		visited.append(search_pos)
 		
 		nodes_in_next_layer += 1
+		
+		path_holder[pos].append(search_pos)
+
+func retrace_path():
+	var curr_node = target_pos
+	
+	var taken_path = []
+	
+	while curr_node != start:
+		for key in path_holder.keys():
+			if curr_node in path_holder[key]:
+				taken_path.append(key)
+				curr_node = key
+	taken_path.invert()
+	print("Start at %s to go to %s via \n %s" % [start, target_pos, taken_path])
