8b76998bc0992465c3bd253d99026c8a639d7802
elemein <44098408+elemein@users.noreply.github.com>
2021-07-14 01:29:03 -0400

    changed lighting. removed default light. light now comes from an omnilight on player. light has a script to slowly glow.

---
+++ Assets/Objects/PlayerObjects/Lantern.gd
@@ -0,0 +1,27 @@
+extends OmniLight
+
+var max_range = 15
+var min_range = 10
+var max_energy = 1
+var min_energy = 0.75
+var growing = false
+
+var change_rate = 0.005 # lower is slower
+
+func _ready():
+	pass
+
+func _process(delta):
+	if (growing == false):
+		if (omni_range > min_range) and (light_energy > min_energy):
+			omni_range += -((max_range - min_range) * change_rate)
+			light_energy += -((max_energy - min_energy) * change_rate)
+		else: 
+			growing = true
+	
+	if (growing == true):
+		if (omni_range < max_range) and (light_energy < max_energy):
+			omni_range += ((max_range - min_range) * change_rate)
+			light_energy += ((max_energy - min_energy) * change_rate)
+		else: 
+			growing = false
