a0a1799bd2000b545ca2de47c6129293eed064da
Debonairesnake6 <canadianboyz@hotmail.ca>
2021-09-09 20:27:33 -0400

    Removed signals from the actions node

--- Assets/SystemScripts/Actions/Actions.gd
+++ Assets/SystemScripts/Actions/Actions.gd
@@ -1,38 +1,61 @@
 extends Node
 
 # Attacks
-signal spell_cast_basic_attack
-signal spell_cast_fireball
+var basic_attack = null
+var fireball = null
 
-signal spell_cast_self_heal
-signal spell_cast_dash
+var self_heal = null
+var dash = null
 
 # Inventory Actions
-signal action_drop_item
-signal action_equip_item
-signal action_unequip_item
+var drop_item = null
+var equip_item = null
+var unequip_item = null
+
+
+
+func _ready():
+	# Attacks
+	basic_attack = find_node('BasicAttack')
+	fireball = find_node('Fireball')
+
+	self_heal = find_node('SelfHeal')
+	dash = find_node('Dash')
+
+	# Inventory Actions
+	drop_item = find_node('DropItem')
+	equip_item = find_node('EquipItem')
+	unequip_item = find_node('UnequipItem')
+
 
 # Attacks
 func _on_spell_cast_basic_attack():
-	emit_signal("spell_cast_basic_attack")
+	basic_attack.use()
+
 	
 func _on_spell_cast_fireball():
-	emit_signal("spell_cast_fireball")
+	fireball.use()
+
 
 # Inventory Actions
 func _on_action_drop_item():
-	emit_signal("action_drop_item")
+	drop_item.use()
+
 
 func _on_action_equip_item():
-	emit_signal("action_equip_item")
+	equip_item.use()
+
 	
 func _on_action_unequip_item():
-	emit_signal("action_unequip_item")
+	unequip_item.use()
+
 
 # Movement
 func _on_spell_cast_dash():
-	emit_signal("spell_cast_dash")
+	dash.use()
+
 
 # Buffs
 func _on_spell_cast_self_heal():
-	emit_signal("spell_cast_self_heal")
+	self_heal.use()
+
