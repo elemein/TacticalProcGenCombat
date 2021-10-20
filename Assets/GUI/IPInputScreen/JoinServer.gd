6ecfea9e3e5a91e88bd7ff3f10d688ee906ab56c
Debonairesnake6 <canadianboyz@hotmail.ca>
2021-10-20 18:10:34 -0400

    added loading icon when attempting to connect to a server

--- Assets/GUI/IPInputScreen/JoinServer.gd
+++ Assets/GUI/IPInputScreen/JoinServer.gd
@@ -1,5 +1,7 @@
 extends Button
 
+onready var loading_icon : TextureRect = get_parent().find_node('Loading')
+
 export var reference_path = 'res://Assets/GUI/CharacterSelect/CharacterSelect.tscn'
 export(bool) var start_focused = false
 
@@ -18,10 +20,17 @@ func _on_button_pressed():
 	Client.set_server_ip(get_parent().find_node('ServerIPInput').text)
 	Client.connect_to_server()
 	
+	loading_icon.visible = true
+	while loading_icon.visible:
+		loading_icon.rect_rotation = loading_icon.rect_rotation + 1
+		yield(get_tree(), "idle_frame")
+	
 func on_successful_connect():
+	loading_icon.visible = false
 	reference_path = 'res://Assets/GUI/CharacterSelect/CharacterSelect.tscn'
 	var _result = get_tree().change_scene(reference_path)
 
 func on_unsuccessful_connect():
+	loading_icon.visible = false
 	print("Failed to join")
 	get_parent().find_node('ServerIPInput').text = 'Failed To Connect'
