a2f26605ac79cfd14d5ae0aa7f1c403ccf465abe
elemein <44098408+elemein@users.noreply.github.com>
2021-11-03 18:42:45 -0400

    Refactor: Cleaned up PSideTurnTimer.gd.

--- Assets/SystemScripts/PSideTurnTimer.gd
+++ Assets/SystemScripts/PSideTurnTimer.gd
@@ -2,13 +2,6 @@ extends Node
 
 var turn_timer = Timer.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _init():
-	pass
-	
 func get_turn_in_process() -> bool:
 	if turn_timer.get_time_left() > 0: return true
 	else: return false
